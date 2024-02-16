7a72c51e966a74c86b9460e8c568fbbe265bcd36
oeduardoramalho <138023476+oeduardoramalho@users.noreply.github.com>
2024-02-16 18:20:59 -0300

    janela modal appearance update

--- script/modal.js
+++ script/modal.js
@@ -3,7 +3,30 @@ const janelaModal = document.querySelector('#janela-modal')
 const aviso = document.querySelector('.aviso')
 const modalTexto = document.querySelector('.modal-texto')
 
-modalTexto.innerHTML = aviso.innerHTML
+const dataFinal = new Date("Feb 20 2024").getTime()
+const dataFinalOntem = dataFinal - 86400000
+const dataFinalAmanha = dataFinal + 86400000
+const dataInicial = dataFinal - (86400000 * 5)
+
+var hoje = new Date().getTime()
+
+
+if (hoje >= dataInicial && hoje < dataFinalAmanha) {
+    janelaModal.classList.remove('disabled')
+} else {
+    janelaModal.classList.add('disabled')
+}
+
+if (hoje >= dataFinalOntem && hoje <= dataFinal) {
+    modalTexto.innerHTML = `Reunião Geral <span style="text-decoration: underline">AMANHÃ</span> (sexta-feira) às 20h<br>
+    Local: Igreja`
+} else if (hoje >= dataFinal && hoje <= dataFinalAmanha) {
+    modalTexto.innerHTML = `Reunião Geral <span style="text-decoration: underline">HOJE</span> às 20h<br>
+    Local: Igreja`
+} else {
+    modalTexto.innerHTML = aviso.innerHTML
+}
+
 
 function fecharModal() {
     janelaModal.style.animation = 'fade-out .2s forwards';
