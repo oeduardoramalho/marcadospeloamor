97d5495b9aee3fa3e16bc9e8a5010cd5ef3f2654
oeduardoramalho <138023476+oeduardoramalho@users.noreply.github.com>
2024-02-16 18:25:49 -0300

    update

--- script/modal.js
+++ script/modal.js
@@ -3,7 +3,7 @@ const janelaModal = document.querySelector('#janela-modal')
 const aviso = document.querySelector('.aviso')
 const modalTexto = document.querySelector('.modal-texto')
 
-const dataFinal = new Date("Feb 23 2024").getTime()
+const dataFinal = new Date("Feb 20 2024").getTime()
 const dataFinalOntem = dataFinal - 86400000
 const dataFinalAmanha = dataFinal + 86400000
 const dataInicial = dataFinal - (86400000 * 5)
@@ -13,8 +13,10 @@ var hoje = new Date().getTime()
 
 if (hoje >= dataInicial && hoje < dataFinalAmanha) {
     janelaModal.classList.remove('disabled')
+    aviso.style.color = 'var(--preto)'
 } else {
     janelaModal.classList.add('disabled')
+    aviso.style.color = 'var(--vermelho)'
 }
 
 if (hoje >= dataFinalOntem && hoje <= dataFinal) {
