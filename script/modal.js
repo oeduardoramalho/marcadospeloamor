ba94fa8feb15bc6ad91babc8e9c455e3a52680a5
oeduardoramalho <138023476+oeduardoramalho@users.noreply.github.com>
2024-02-22 14:27:08 -0300

    content update

--- script/modal.js
+++ script/modal.js
@@ -2,7 +2,7 @@ const janelaModal = document.querySelector('#janela-modal')
 const aviso = document.querySelector('.aviso')
 const modalTexto = document.querySelector('.modal-texto')
 
-const dataFinal = new Date("Feb 23 2024").getTime()
+const dataFinal = new Date("Mar 01 2024").getTime()
 const dataFinalOntem = dataFinal - 86400000
 const dataFinalAmanha = dataFinal + 86400000
 const dataInicial = dataFinal - (86400000 * 4)
